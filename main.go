9b3ab8f3c27e9e66df129d65ea28d52ef3afdbff
Ben Connor <codingunderthehood@gmail.com>
2021-04-23 10:06:15 +0300

    Add echo service

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/codingunderthehood/go-course/smetanin"
+	"log"
+	"net/http"
+	"os"
 )
 
+const DefaultPort = "7893"
+
+func getServerPort() string {
+	port := os.Getenv("SERVER_PORT")
+	if port != "" {
+		return port
+	}
+
+	return DefaultPort
+}
+
+func echoHandler(writer http.ResponseWriter, request *http.Request) {
+	log.Println("Echoing back request made to " + request.URL.Path + " to client (" + request.RemoteAddr + ")")
+
+	writer.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// allow pre-flight headers
+	writer.Header().Set("Access-Control-Allow-Headers", "Content-Range, Content-Disposition, Content-Type, ETag")
+
+	request.Write(writer)
+}
+
 func main() {
-	fmt.Println(smetanin.SolutionSquareGenerator(-5, 10))
+	log.Println("starting server, listening on port " + getServerPort())
+
+	http.HandleFunc("/", echoHandler)
+	http.ListenAndServe(":"+getServerPort(), nil)
 }
