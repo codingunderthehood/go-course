905429ee8357555860f8970d7b5fef94c77ed491
Ben Connor <codingunderthehood@gmail.com>
2021-04-23 15:17:16 +0300

    Remove useless print in Uniq solution

--- smetanin/Uniq.go
+++ smetanin/Uniq.go
@@ -1,14 +1,10 @@
 package smetanin
 
-import "fmt"
-
 func SolutuionUniq(A []int) int {
 	m := make(map[int]bool)
 	for _, element := range A {
 		m[element] = true
 	}
-
-	fmt.Println(m)
 
 	return len(m)
 }
